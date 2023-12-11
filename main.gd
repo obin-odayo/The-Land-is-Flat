bef895322f031c1ab8b933c75c0934b1506e143a
Z1aaan <zheanrobbyganituen@gmail.com>
2023-12-12 01:15:49 +0800

    Added functional buttons to main menu

---
+++ main.gd
@@ -0,0 +1,33 @@
+# Created by: 	Zhean Robby L. Ganituen 
+#				CRATE
+# Created on:	DEC 12, 2023
+# Last Updated:	DEC 12, 2023
+# Created for: 	The Land is Flat
+
+extends Button
+
+func _on_button_play_pressed():
+	# when play is pressed
+	# start at the first scene of the game
+	
+	# format: get_tree().change_scene_to_file("res://<main>.tscn")
+	pass # Replace with function body.
+
+func _on_button_options_pressed():
+	# when options is pressed
+	# show game options menu
+	
+	# format: get_tree().change_scene_to_file("res://<options_scene>.tscn")
+	pass # Replace with function body.
+
+func _on_buttton_credits_pressed():
+	# when credits is pressed
+	# show the game credits
+	
+	# format: get_tree().change_scene_to_file("res://<credits_scene>.tscn")
+	pass # Replace with function body.
+
+func _on_button_exit_pressed():
+	# when exit button is pressed
+	# exit the game
+	get_tree().quit()
