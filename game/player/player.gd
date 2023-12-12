6556b0a81db9f6e0327aa2777a42497b45591676
Z1aaan <zheanrobbyganituen@gmail.com>
2023-12-12 17:11:31 +0800

    Player Script Updates
    Added WASD as movement in controls in `project.godot`
    Used `MoveLeft` instead of `ui_left`, etc.
    Added `anim.play("")` for walking and idle animations.

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,4 +1,13 @@
+# Created by: 	Zhean Robby L. Ganituen 
+#				CRATE
+# Created on:	DEC 12, 2023
+# Last Updated:	DEC 12, 2023
+# Created for: 	The Land is Flat
+# PLAYER SCRIPT
+
 extends CharacterBody2D
+
+@onready var anim = get_node("AnimationPlayer")
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -7,21 +16,25 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("MoveUp") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("MoveLeft", "MoveRight")
+	
+	# move player depending on which button is pressed.
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("Idle")
 
 	move_and_slide()
