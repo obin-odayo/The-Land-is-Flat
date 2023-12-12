c5162bbb1946aaa5967ad248c266f08d7ae8adef
Z1aaan <zheanrobbyganituen@gmail.com>
2023-12-12 17:40:18 +0800

    Added sprite rotation based on movement

--- game/player/player.gd
+++ game/player/player.gd
@@ -21,21 +21,29 @@ func _physics_process(delta):
 	
 	# move player depending on which button is pressed.
 	# if a movement key is pressed rotate the animation based on the movement key pressed.
+	# solution: rotation = lerp_angle(rotation,<rotation in radians>,rotationSpeed)
+	var rotationSpeed = 0.2
 	if Input.is_action_pressed("MoveLeft"):
 		# if "A" is pressed move the character to the left based on the negative of speed.
 		# negative because in the (x,y) plane to move to the left we need to move in the negative direction.
 		velocity.x = -1 * SPEED
+		rotation = lerp_angle(rotation,-PI/2,rotationSpeed)
 		anim.play("Walk")
 	elif Input.is_action_pressed("MoveRight"):
 		# if "D" is pressed move the character to the right.
 		velocity.x = 1 * SPEED
+		rotation = lerp_angle(rotation,PI/2,rotationSpeed)
 		anim.play("Walk")
 	elif Input.is_action_pressed("MoveUp"):
 		# if "W" is pressed move the character to the up.
 		velocity.y = -1 * SPEED
+		rotation = lerp_angle(rotation,0,rotationSpeed)
+		anim.play("Walk")
 	elif Input.is_action_pressed("MoveDown"):
 		# if "S" is pressed move the character to the down.
 		velocity.y = 1 * SPEED
+		rotation = lerp_angle(rotation,PI,rotationSpeed)
+		anim.play("Walk")
 	else:
 		# if no buttons are pressed make x and y velocity 0 and play Idle.
 		velocity.x = move_toward(velocity.x, 0, SPEED)
